32d43a1e152fce2facb0b1207b5f3ca72f89da25
luksm770 <lukas@lukassmith.se>
2025-09-29 14:46:23 +0200

    Fixed

--- md.py
+++ md.py
@@ -12,8 +12,8 @@ def calcenergy(a):
     etot = epot + ekin
     temp = 2/3*ekin*1/units.kB
 
-    return "hej"
-    #return epot, ekin, etot, temp
+    #return "hej"
+    return epot, ekin, etot, temp
 
 
 
